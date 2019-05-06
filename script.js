7a972f261a2e80c6f426f0be02fee6e1950a98f7
boredasfawk <olonnyetaylor@gmail.com>
2019-05-05 23:11:42 -0700

    update css/add slideshow/add js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-$(document).ready(function () {
-  $('.btn').click(function () {
-    $('nav ul').slideToggle(3000)
-  })
+(function IIFE() {
+  let slideIndex = 0;
+  let i;
+  const slides = document.getElementsByClassName("slide_fade");
 
-})
+  function showSlides() {
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    return setTimeout(showSlides, 4000);
+  }
+  return showSlides;
+})()();
+
+$(document).ready(function() {
+  $(".btn").click(function() {
+    $("nav ul").slideToggle(3000);
+  });
+});
