7540489b1353996d2f037c4e8588cbe474496986
boredasfawk <olonnyetaylor@gmail.com>
2019-05-06 23:56:31 -0700

    update js for nav/update css for image position

--- script.js
+++ script.js
@@ -19,8 +19,38 @@
   return showSlides;
 })()();
 
+const media = window.matchMedia("(max-width: 690px)");
 const navUl = document.getElementById("navHide");
 const btn = document.getElementById("btn");
+const li = document.querySelector("li");
+
+if (media.mathes) {
+  navUl.style.display = "none";
+}
+
+var getParentAnchor = function(element) {
+  while (element !== null) {
+    if (element.tagName.toUpperCase() === "A") {
+      return element;
+    }
+    element = element.parentNode;
+  }
+  return null;
+};
+document.querySelector("body").addEventListener(
+  "click",
+  function(e) {
+    var anchor = getParentAnchor(e.target);
+    if (anchor !== null) {
+      if (navUl.style.display === "none") {
+        navUl.style.display = "block";
+      } else {
+        navUl.style.display = "none";
+      }
+    }
+  },
+  false
+);
 
 window.onload = () => {
   btn.addEventListener("click", () => {
